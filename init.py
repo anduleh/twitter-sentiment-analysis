cdf7824748b909890c834b31f45758beaa49a02e
Andrew <andrewmyli96@gmail.com>
2019-07-01 17:14:29 -0700

    final

--- init.py
+++ init.py
@@ -1,9 +1,19 @@
 import tweepy
 from textblob import TextBlob
-import keys
+from keys import *
+from pprint import pprint
 
-auth = tweepy.OAuthHandler(keys.consumer_key, keys.consumer_secret)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
+
+public_tweets = api.search('Trump')
+
+for tweet in public_tweets:
+	text = tweet.text
+	analysis = TextBlob(text)
+	print(text)
+	print(analysis.sentiment)
+	print('-'*40)
 
